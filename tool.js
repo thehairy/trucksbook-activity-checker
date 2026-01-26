7fe2512d77de0c6bd0bf1873d459df595db14407
Sören Stabenow <soeren.stabenow@outlook.com>
2026-01-26 13:23:11 +0100

    rename

--- tool.js
+++ tool.js
@@ -246,7 +246,7 @@ function downloadExcelCSV(data) {
     const encodedUri = encodeURI(csvContent);
     const link = document.createElement("a");
     link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "trucksbook_activity_final.csv");
+    link.setAttribute("download", "trucksbook_activity.csv");
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
